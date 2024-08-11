01d7533145c87d65e4a4ba83b52209c0f5f2d069
ldxhyeon <dlehdgus3180@naver.com>
2024-08-11 20:07:06 +0900

    2024_08_11  JSP,Servlet을 이용한 TodoList 프로그램

--- TodoList/src/main/java/edu/kh/todolist/controller/MainServlet.java
+++ TodoList/src/main/java/edu/kh/todolist/controller/MainServlet.java
@@ -1,20 +1,24 @@
 package edu.kh.todolist.controller;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+import edu.kh.todolist.dto.Todo;
+import edu.kh.todolist.service.TodoListService;
+import edu.kh.todolist.service.TodoListServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-// "/" 요청을 매핑하여 처리하는 서블릿
-// "/" == localhost/ ==  메인페이지
-
-@WebServlet("/")
+// "/main" 요청을 매핑하여 처리하는 서블릿
+// "/main" == localhost/main ==  메인페이지
+@WebServlet("/main")
 public class MainServlet extends HttpServlet{
 
-	/* 왜 "/" 메인페이지 요청을 처리하는 서블릿 만들었는가 ??
+	/* 왜 "/main" 메인페이지 요청을 처리하는 서블릿 만들었는가 ??
 	 * - Servlet(Back-end)에서 추가한 데이터를 
 	 * 메인 페이지에서 사용할 수 있게 하려고         
 	 * 
@@ -27,13 +31,38 @@ public class MainServlet extends HttpServlet{
 		// str 키 값을 찾아 값 Set 하기
 		// req.s etAttribute("str", "메인 페이지 요청 시 값 전달됨");
 		
+		try {
+			// Service 객체 생성
+			TodoListService service = new TodoListServiceImpl();
+			
+			// 전체 할 일 목록 + 완료된 Todo 개수가 담긴 Map을
+			// Service 호출해서 얻어오기
+			Map<String, Object> map = service.todoListFullView();																																																																										
+			
+			// Map에 저장된 값 풀어내기
+			List<Todo> todoList = (List<Todo>)map.get("todoList");																									
+			int completeCount = (int)map.get("completeCount");
+			
+			// reqquest scope 객체 																																																																																																																																																														값을 추가
+			req.setAttribute("todoList",todoList);
+			req.setAttribute("completeCount",completeCount);
+			
+			
+			// 메인 페이지 응답을 담당하는 jsp에 요청 위임
+			// 처리할 경로 정하기
+			String path = "/WEB-INF/views/main.jsp";
+			// 경로에 응답과 요청 위임
+			req.getRequestDispatcher(path).forward(req, resp);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		
-		// 처리할 경로 정하기
-		String path = "/WEB-INF/views/main.jsp";
 		
-		// 경로에 응답과 요청 위임
-		req.getRequestDispatcher(path).forward(req, resp);
+		
+		
+		
 		
 		
 		
